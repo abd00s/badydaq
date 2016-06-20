f2fe81cb0d56808e766391a5bcd59e98a62e6c2e
Abdullah <abd00s@users.noreply.github.com>
2016-06-20 19:05:09 +0300

    Generate model bid

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620155413) do
+ActiveRecord::Schema.define(version: 20160620160432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bids", force: :cascade do |t|
+    t.float    "value"
+    t.float    "interest"
+    t.boolean  "accepted?"
+    t.integer  "lender_id"
+    t.integer  "exchange_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string   "location"
